24f39681f74d4b9424422288b623c10a01579f21
tjangxu <tjangnan@gmail.com>
2020-04-17 16:58:37 +0200

    setup

---
+++ war/src/main/java/com.teamwest.parkshark/ParksharkApplication.java
@@ -0,0 +1,17 @@
+package com.teamwest.parkshark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableSwagger2
+public class ParksharkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParksharkApplication.class, args);
+	}
+
+}
