1722d2657fc09110f773c99d8e0985c84f06ef08
tjangxu <tjangnan@gmail.com>
2020-04-17 17:00:45 +0200

    setup

---
+++ war/src/test/java/com/teamwest/parkshark/ParksharkApplicationTest.java
@@ -0,0 +1,13 @@
+package com.teamwest.parkshark;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParksharkApplicationTest {
+
+    @Test
+    void contextTest() {
+
+    }
+}
