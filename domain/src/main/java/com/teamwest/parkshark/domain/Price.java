91aecd54331839002d968f9653a7ada6fbc19892
tjangxu <tjangnan@gmail.com>
2020-04-20 10:11:25 +0200

    POJOs domain: parkinglot

---
+++ domain/src/main/java/com/teamwest/parkshark/domain/Price.java
@@ -0,0 +1,19 @@
+package com.teamwest.parkshark.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Price {
+
+    @Column(name = "amount")
+    private double amount;
+
+    @Column(name = "unit")
+    private String unit;
+
+    public Price(double amount, String unit) {
+        this.amount = amount;
+        this.unit = unit;
+    }
+}
---
+++ domain/src/main/java/com/teamwest/parkshark/domain/parkinglots/Parkinglot.java
@@ -0,0 +1,47 @@
+package com.teamwest.parkshark.domain.parkinglots;
+
+import com.teamwest.parkshark.domain.Price;
+
+import javax.persistence.*;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Table(name = "parkinglot")
+public class Parkinglot {
+
+    @Id
+    @GeneratedValue(strategy = SEQUENCE, generator = "PARKINGLOT_SEQ" )
+    @SequenceGenerator(name = "PARKINGLOT_SEQ", sequenceName = "parkinglot_seq", allocationSize = 1)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private ParkinglotCategory parkinglotCategory;
+
+    @Column(name = "capacity")
+    private long capacity;
+
+    @JoinColumn(name = "fk_person_id")
+    private Person contactPerson;
+
+    @JoinColumn(name = "fk_address_id")
+    private Address address;
+
+    @Embedded
+    private Price pricePerHour;
+//    TODO private Division division
+
+
+    public Parkinglot(String name, ParkinglotCategory parkinglotCategory, long capacity, Person contactPerson, Address address, Price pricePerHour) {
+        this.name = name;
+        this.parkinglotCategory = parkinglotCategory;
+        this.capacity = capacity;
+        this.contactPerson = contactPerson;
+        this.address = address;
+        this.pricePerHour = pricePerHour;
+    }
+}
