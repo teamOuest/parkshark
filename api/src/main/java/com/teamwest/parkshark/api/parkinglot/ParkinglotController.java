7b843cabb95a3637476be0adb03de274ae43385a
tjangxu <tjangnan@gmail.com>
2020-04-20 15:13:10 +0200

    Parklot api

---
+++ api/src/main/java/com/teamwest/parkshark/api/parkinglot/ParkinglotController.java
@@ -0,0 +1,26 @@
+package com.teamwest.parkshark.api.parkinglot;
+
+import com.teamwest.parkshark.service.parkinglot.CreateParkinglotDto;
+import com.teamwest.parkshark.service.parkinglot.ParkinglotDto;
+import com.teamwest.parkshark.service.parkinglot.ParkinglotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "parkinglot")
+public class ParkinglotController {
+
+    ParkinglotService parkinglotService;
+
+    @Autowired
+    public ParkinglotController(ParkinglotService parkinglotService) {
+        this.parkinglotService = parkinglotService;
+    }
+
+    @PostMapping(consumes="application/json", produces="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ParkinglotDto createParkinglot(@RequestBody CreateParkinglotDto createParkinglotDto){
+        return parkinglotService.createParkingLot(createParkinglotDto);
+    }
+}
--- domain/src/main/java/com/teamwest/parkshark/domain/member/Employee.java
+++ domain/src/main/java/com/teamwest/parkshark/domain/member/Employee.java
@@ -4,6 +4,70 @@ import javax.persistence.*;
 
 @Entity
 @Table(name="employee")
-public class Employee extends Person {
+public class Employee {
+
+    @Id
+    @SequenceGenerator(allocationSize = 1, name = "PERSON_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "PERSON_SEQ")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "countryCode", column = @Column(name = "countryCodeMobile")),
+            @AttributeOverride(name = "localNumber", column = @Column(name = "localNumberMobile"))
+    })
+    private PhoneNumber mobilePhoneNumber;
+
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "countryCode", column = @Column(name = "countryCodeLandline")),
+            @AttributeOverride(name = "localNumber", column = @Column(name = "localNumberLandline"))
+    })
+    private PhoneNumber landlinePhoneNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Embedded
+    private Address address;
+
+    public Employee() {
+
+    }
+
+    public Employee(String name, PhoneNumber mobilePhoneNumber, PhoneNumber landlinePhoneNumber, String email, Address adress) {
+        this.name = name;
+        this.mobilePhoneNumber = mobilePhoneNumber;
+        this.landlinePhoneNumber = landlinePhoneNumber;
+        this.email = email;
+        this.address = adress;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PhoneNumber getMobilePhoneNumber() {
+        return mobilePhoneNumber;
+    }
+
+    public PhoneNumber getLandlinePhoneNumber() {
+        return landlinePhoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
 
 }
