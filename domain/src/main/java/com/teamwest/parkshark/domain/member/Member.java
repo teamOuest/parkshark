33ab62ee5dca9f82f1d82cff68051f8ef11f871b
maarteneeckman <maarten_eeckman@hotmail.com>
2020-04-20 14:25:01 +0200

    Finished story ME1: register as a member

--- domain/src/main/java/com/teamwest/parkshark/domain/member/Member.java
+++ domain/src/main/java/com/teamwest/parkshark/domain/member/Member.java
@@ -1,26 +1,80 @@
 package com.teamwest.parkshark.domain.member;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+
+import static javax.persistence.GenerationType.SEQUENCE;
 
 @Entity
 @Table(name="member")
-public class Member extends Person{
+public class Member{
 
-    @Column(name="licensePlate")
+    @Id
+    @Column(name="id")
+    @SequenceGenerator(allocationSize = 1, name = "MEMBER_SEQ", sequenceName="member_seq")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "MEMBER_SEQ")
+    private int id;
+    @Column(name = "name")
+    private String name;
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "countryCode", column = @Column(name = "countrycodemobile")),
+            @AttributeOverride(name = "localNumber", column = @Column(name = "localnumbermobile"))
+    })
+    private PhoneNumber mobilePhoneNumber;
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "countryCode", column = @Column(name = "countrycodelandline")),
+            @AttributeOverride(name = "localNumber", column = @Column(name = "localnumberlandline"))
+    })
+    private PhoneNumber landlinePhoneNumber;
+    @Column(name = "email")
+    private String email;
+    @Embedded
+    private Address address;
+
+    @Column(name="licenseplate")
     private String licensePlate;
-    @Column(name="registrationDate")
+    @Column(name="registrationdate")
     private LocalDate registrationDate;
 
     public Member(){}
 
-    public Member(String name, PhoneNumber mobileNumber, PhoneNumber landlineNumber, String email, Address address , String licensePlate, LocalDate registrationDate) {
-        super(name, mobileNumber, landlineNumber, email, address);
+    public Member(String name, PhoneNumber mobilePhoneNumber, PhoneNumber landlinePhoneNumber, String email, Address address, String licensePlate, LocalDate registrationDate) {
+        this.id = id;
+        this.name = name;
+        this.mobilePhoneNumber = mobilePhoneNumber;
+        this.landlinePhoneNumber = landlinePhoneNumber;
+        this.email = email;
+        this.address = address;
         this.licensePlate = licensePlate;
         this.registrationDate = registrationDate;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public PhoneNumber getMobilePhoneNumber() {
+        return mobilePhoneNumber;
+    }
+
+    public PhoneNumber getLandlinePhoneNumber() {
+        return landlinePhoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
 
 
     public String getLicensePlate() {
@@ -30,4 +84,6 @@ public class Member extends Person{
     public LocalDate getRegistrationDate() {
         return registrationDate;
     }
+
+
 }
