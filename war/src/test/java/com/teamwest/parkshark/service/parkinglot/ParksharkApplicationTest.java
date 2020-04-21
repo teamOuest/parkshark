4139316287d9e3ebe5936ee112e6dd06394d8ad8
BramVanMechelen <bram.vm@hotmail.com>
2020-04-21 22:15:07 +0200

    simple integrationtest

--- war/src/test/java/com/teamwest/parkshark/service/parkinglot/ParksharkApplicationTest.java
+++ war/src/test/java/com/teamwest/parkshark/service/parkinglot/ParksharkApplicationTest.java
@@ -1,7 +1,16 @@
 package com.teamwest.parkshark.service.parkinglot;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.net.http.HttpResponse;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,6 +19,14 @@ class ParksharkApplicationTest {
 
     @Test
     void springBootTestContext() {
+    }
 
+    @Test
+    public void atTest() throws IOException
+    {
+        HttpUriRequest request = new HttpGet("http://localhost:8080/parkinglot");
+        StatusLine statusLine = HttpClientBuilder.create().build().execute(request).getStatusLine();
+        System.out.println(statusLine.getStatusCode());
+        assertEquals(HttpStatus.SC_OK, statusLine.getStatusCode());
     }
 }
