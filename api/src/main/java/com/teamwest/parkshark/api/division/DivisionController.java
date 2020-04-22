727fe5038aa5e264de04b247534ecd29199a8e84
BramVanMechelen <bram.vm@hotmail.com>
2020-04-22 16:30:56 +0200

    create division

---
+++ api/src/main/java/com/teamwest/parkshark/api/division/DivisionController.java
@@ -0,0 +1,27 @@
+package com.teamwest.parkshark.api.division;
+
+import com.teamwest.parkshark.service.division.CreateDivisionDto;
+import com.teamwest.parkshark.service.division.DivisionDto;
+import com.teamwest.parkshark.service.division.DivisionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="divisions")
+public class DivisionController {
+
+    DivisionService divisionService;
+
+    @Autowired
+    DivisionController(DivisionService divisionService){
+        this.divisionService = divisionService;
+    }
+
+    @PostMapping(consumes="application/json", produces="application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public DivisionDto createDivision(@RequestBody CreateDivisionDto createDivisionDto){
+        return divisionService.createDivision(createDivisionDto);
+    }
+
+}
